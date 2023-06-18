c714e25b9719e2a9186436d71ff80ab730e9ad81
Nguyễn Quang Gia Thuận <GiaThuanNguyen213@gmail.com>
2023-06-18 21:41:33 +0700

    Can give a list of image which relative to the given image

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,12 +1,62 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
+import React from "react"
+import axios from 'axios';
 export default function Home() {
-  
+  const [imageFile, SetImageFile] = React.useState("");
+  const [data, SetData] = React.useState([])
+  const InputEle = React.useRef();
+  const HandlePredictImage = async function (fileImage: File) {
+    try {
+      let formData = new FormData();
+      formData.append("image", fileImage);
+      let result = await axios.post(`http://127.0.0.1:5000/upload`, formData,
+        {
+          headers: { "Content-Type": "multipart/form-data", 'Access-Control-Allow-Origin': '*' },
+        })
+      SetData(result.data)
+      console.log(result.data[0])
+    } catch (error) {
+
+    }
+  }
   return (
     <>
-        <input type="file"  id="" />
+      <div className='w-screen h-screen '>
+        <div className='h-[200px] flex justify-center flex-col items-center mb-10'>
+          <div className='w-[200px] h-[200px] overflow-hidden'>
+            <img src={imageFile}
+              className=' w-full h-auto object-contain' alt="" />
+          </div>
+          <input onChange={async (e) => {
+            let files = e.target.files as any
+            console.log()
+            SetImageFile(URL.createObjectURL(files[0] as Blob))
+            await HandlePredictImage(e.target.files?.item(0) as File)
+          }} type="file" id="" />
+        </div>
+
+        <div className='px-3 py-2 flex flex-wrap'>
+          {
+            data.map((item, index) => {
+              return <>
+                <div className='basis-1/5 md:basis-1/4 basis-1/2 h-[300px] p-2'>
+                  <div className='h-full w-full'>
+                    <img className='h-full w-full object-contain' src={`http://127.0.0.1:5000/images/${item[0].replace("./datasets/images_original\\","")}`} alt="" />
+                  </div>
+                </div>
+              </>
+            })
+          }
+
+        </div>
+
+
+
+      </div>
+
+
     </>
   )
 }
--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 import os
 import io
-from flask import Flask,request,Response
+from flask import Flask,request,Response,send_from_directory
+from flask_cors import cross_origin,CORS
 from PIL import Image
 from search_image import get_extract_model,extract_vector
 import pickle
@@ -12,13 +13,25 @@ from flask import jsonify,json
 script_location = Path(__file__).absolute().parent
 
 app = Flask(__name__)
+CORS(app, resources={r"/*":{"origins":"*"}})
 
+@app.route('/images/<path:filename>')
+def serve_image(filename):
+    # Specify the directory where your images are located
+    directory = 'datasets/images_original'
+   #  "./datasets/images_original\\708af227-43ba-4e2b-b35b-652a13cd1c77.jpg"
+    # Use the send_from_directory function to serve the image
+    return send_from_directory(directory, filename)
+
+@cross_origin
 @app.route('/upload', methods = ['GET', 'POST'])
+
 def upload_file():
    image_file = request.files['image']
    image = Image.open(image_file)
-   # file_location = script_location / 'vectors.pkl'
-   # file = file_location.open()
+   # image.show();
+   file_location = script_location / 'vectors.pkl'
+   file = file_location.open()
    model = get_extract_model()
    search_vector = extract_vector(model, image_file)  
    vectors = pickle.load(open("vectors.pkl","rb"))
@@ -27,37 +40,21 @@ def upload_file():
    K = 16
    ids = np.argsort(distance)[:K]
    nearest_image = [(paths[id], distance[id]) for id in ids]
-   for i in nearest_image:
-       print(i.__class__)
-       
-           
-   # axes = []
-   # grid_size = int(math.sqrt(K))
-   # fig = plt.figure(figsize=(10,5))
-   print(nearest_image)
-   # axes = []
-   # for id in range(K):
-   #    draw_image = nearest_image[id]
-   #    axes.append(fig.add_subplot(grid_size, grid_size, id+1))
-      
-   #    axes[-1].set_title(draw_image[1])
-   #    plt.imshow(Image.open(draw_image[0]))
+   axes = []
+   grid_size = int(math.sqrt(K))
+   fig = plt.figure(figsize=(10,5))
+   
 
-   # fig.tight_layout()
+   for id in range(K):
+       draw_image = nearest_image[id]
+       axes.append(fig.add_subplot(grid_size, grid_size, id+1))
+      
+       axes[-1].set_title(draw_image[1])
+       plt.imshow(Image.open(draw_image[0]))
+
+   fig.tight_layout()
    # plt.show()
 
-    # Example: Resize the image
-   # resized_image = image.resize((800, 600))
-   
-   #  # Example: Save the processed image
-   # resized_image.save('output.jpg')
-    
-   # return jsonify(nearest_image)
-   # response = app.response_class(
-   #      response=json.dumps(nearest_image),
-   #      status=200,
-   #      mimetype='application/json'
-   #  )
    serialized_list = [(path, float(value)) for path, value in nearest_image]
    return jsonify(serialized_list)
 
